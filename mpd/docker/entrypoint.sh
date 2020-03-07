113c3dada2d4589711d2f11e5f51230991b62f36
German Antsiferov <dxdy@bk.ru>
2020-03-07 19:16:10 +0000

    Add entrypoint to docker Mpd

--- mpd/docker/entrypoint.sh
+++ mpd/docker/entrypoint.sh
@@ -4,7 +4,10 @@
 amixer cset numid=3 1
 
 # Set default queue
-mpc clear
-ls /mpd/playlists/*.m3u | xargs cat | xargs mpc insert
+(
+  sleep 5
+  mpc clear
+  ls /mpd/playlists/*.m3u | xargs cat | xargs mpc insert
+) &
 
 /usr/bin/mpd --no-daemon --stdout /mpd/conf/mpd.conf
