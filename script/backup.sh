02345b65481df2c1d75315ff06a1e0a7d52d014c
German Antsiferov <dxdy@bk.ru>
2020-05-31 18:14:29 +0100

    Add database to backup

--- script/backup.sh
+++ script/backup.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+set -e
+
 read -p "Enter login: " LOGIN
-read -p "Enter password: " PASSWORD
+read -s -p "Enter password: " PASSWORD
 
 ENDPOINT="https://webdav.yandex.ru/dump"
 BACKUP_NAME=my_smart_home_$(date +"%Y%m%d_%H%M%S")
@@ -15,13 +17,15 @@ sudo cp duckdns/.env $BACKUP_NAME/duckdns/.env
 sudo cp homeassistant/config/secrets.yaml $BACKUP_NAME/homeassistant/config/secrets.yaml
 sudo cp homeassistant/config/html5_push_registrations.conf $BACKUP_NAME/homeassistant/config/html5_push_registrations.conf
 sudo cp homeassistant/config/known_devices.yaml $BACKUP_NAME/homeassistant/config/known_devices.yaml
-#sudo cp homeassistant/config/home-assistant_v2.db $BACKUP_NAME/homeassistant/config/home-assistant_v2.db
+sudo cp homeassistant/config/home-assistant_v2.db $BACKUP_NAME/homeassistant/config/home-assistant_v2.db
 sudo cp -r homeassistant/config/.storage/ $BACKUP_NAME/homeassistant/config/.storage/
 
 sudo cp zigbee2mqtt/data/configuration.yaml $BACKUP_NAME/zigbee2mqtt/data/configuration.yaml
 sudo cp zigbee2mqtt/data/state.json $BACKUP_NAME/zigbee2mqtt/data/state.json
 
 sudo cp -r certbot/letsencrypt/ $BACKUP_NAME/certbot/letsencrypt/
+
+sudo cp -r influxdb/ $BACKUP_NAME/influxdb
 
 sudo tar czf $BACKUP_NAME.tar.gz $BACKUP_NAME
 
